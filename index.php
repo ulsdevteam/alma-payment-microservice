2784ce37d7ec61b9bbdc2ee91113ec13ffd765ac
chryslovelace <chryslovelace@gmail.com>
2021-09-03 15:09:02 -0400

    POST endpoint for partial payments

--- index.php
+++ index.php
@@ -5,8 +5,13 @@ use Scriptotek\Alma\Client as AlmaClient;
 use net\authorize\api\contract\v1 as AnetAPI;
 use net\authorize\api\controller as AnetController;
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'OPTIONS') {
     http_response_code(200);
+    exit;
+} else if (!in_array($method, ['GET', 'POST'])) {    
+    http_response_code(400);
     exit;
 }
 
@@ -15,14 +20,46 @@ try {
     if (!$jwt_payload) {
         http_response_code(401);
         exit;
+    }    
+    $alma = new AlmaClient(ALMA_API_KEY, ALMA_REGION);
+    if ($method === 'GET') {        
+        $userId = $jwt_payload->userName;
+    } else if ($method === 'POST') {
+        $userId = $_POST['userId'];
     }
-    $user = getAlmaUser($jwt_payload);
+    $user = $alma->users->get($userId);
     if ($user->fees->total_sum === 0) {
         http_response_code(200);
         echo "You currently have no fines or fees that need to be paid.";
         exit;
     }
-    $token = getAuthorizeTransactionToken($user);
+    if ($method === 'GET') {
+        $fees = [];
+        foreach ($user->fees as $fee) {
+            $fees[$fee->id] = $fee->balance;
+        }
+    } else if ($method === 'POST') {
+        if (!$user->exists()) {
+            http_response_code(400);
+            echo 'User with id "' . $userId . '" was not found.';
+            exit;
+        }
+        $fees = $_POST['fees'];
+        $feeErrors = [];
+        foreach ($fees as $feeId => $amount) {
+            $fee = $user->fees->get($feeId);
+            if ($amount > $fee->balance) {
+                $feeErrors[] = 'Given amount for fee "' . $feeId . '" is higher than the current balance.';
+            }
+            // TODO: other checks for fee amounts not being valid based on fee type, i.e. a fee type that must be paid in full
+        }
+        if (!empty($feeErrors)) {
+            http_response_code(400);
+            echo join("\n", $feeErrors);
+            exit;
+        }
+    }
+    $token = getAuthorizeTransactionToken($user, $fees);
 ?>
 <!DOCTYPE html>
 <html>
@@ -49,9 +86,10 @@ try {
  * Given a user, read their fees and use Authorize.net API to create a token that can be used to show a payment form
  * 
  * @param Scriptotek\Alma\Users\User $user The alma user
+ * @param array $fees The fee amounts: those sent by the user if it's a POST, or the full amount for all fees if it's a GET
  * @return string The token
  */
-function getAuthorizeTransactionToken($user) {
+function getAuthorizeTransactionToken($user, $fees) {
     $transactionRequest = new AnetAPI\TransactionRequestType();
     $transactionRequest->setTransactionType("authCaptureTransaction");
     $transactionRequest->setAmount($user->fees->total_sum);
@@ -68,13 +106,14 @@ function getAuthorizeTransactionToken($user) {
     $transactionRequest->setCustomer($customer);
 
     // set line item id as the fee id, will also be retrieved in the receipt webhook
-    foreach ($user->fees as $fee) {
+    foreach ($fees as $feeId => $amount) {
+        $fee = $user->fees->get($feeId);
         $lineItem = new AnetAPI\LineItemType();
-        $lineItem->setItemId($fee->id);
+        $lineItem->setItemId($feeId);
         $lineItem->setName($fee->type->value);
         $lineItem->setDescription($fee->type->desc);
         $lineItem->setQuantity(1);
-        $lineItem->setUnitPrice($fee->balance);
+        $lineItem->setUnitPrice($amount);
         $transactionRequest->addToLineItems($lineItem);
     }
 
