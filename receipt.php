1b7c28bc5b3b9c93e9af09fccfb36024e5b65271
Chrysanthemum Lovelace <chryslovelace@gmail.com>
2023-08-30 09:19:11 -0400

    remove unneeded comment block

--- receipt.php
+++ receipt.php
@@ -26,39 +26,7 @@ try {
     switch ($notification->eventType) {
         case 'net.authorize.payment.authcapture.created':
             $transactionId = $notification->payload->id;
-
             shell_exec('php record_alma_transaction.php ' . $transactionId . ' 2>&1 | tee -a record_transaction.log 2>/dev/null >/dev/null &');
-/*
-            $request = new AnetApi\GetTransactionDetailsRequest();
-            $request->setMerchantAuthentication(getMerchantAuthentication());
-            $request->setTransId($transactionId);
-
-            // get the transaction details so we can read the fee ids off of the line items
-            $controller = new AnetController\GetTransactionDetailsController($request);
-            $response = $controller->executeWithApiResponse(AUTHORIZE_ENVIRONMENT);
-
-            if ($response == null) {
-                throw new Exception("An error occurred when retrieving transaction details.");
-            } else if ($response->getMessages()->getResultCode() != "Ok") {
-                $errorMessages = $response->getMessages()->getMessage();
-                throw new Exception($errorMessages[0]->getCode() . " " . $errorMessages[0]->getText());        
-            }
-
-            $transaction = $response->getTransaction();
-            $userId = $transaction->getCustomer()->getId();
-            $alma = new AlmaClient(ALMA_API_KEY, ALMA_REGION);
-            foreach ($transaction->getLineItems() as $lineItem) {
-                $feeId = $lineItem->getItemId();
-                // mark each fee as paid
-                $url = $alma->buildUrl('/users/' . $userId . '/fees/' . $feeId, [
-                    'op' => 'pay',
-                    'amount' => strval($lineItem->getUnitPrice()),
-                    'method' => 'ONLINE',
-                    'external_transaction_id' => $transactionId
-                ]);
-                $alma->post($url, null);
-            }
-*/
             break;
         
         default:
